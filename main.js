aaf2bb09b1575e9f323f3cd22a4fc200db6ca55d
fatou <fadia13@yahoo.fr>
2025-12-05 15:30:46 +0100

    toggle burger nav

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+
+const toggle = document.getElementById("burgerToggle");
+const content = document.getElementById("burgerContent");
+
+
+toggle.addEventListener("click" , ()=>{
+
+  content.style.display = content.style.display == "none" ? "block" : "none";
+})
+
+
+
+
+
+
+
+
 //********** */ new***************
 
 // faire descendre le burger 
